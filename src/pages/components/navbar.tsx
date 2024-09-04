946a0fbf1a91b2550b4da80450146ed4262b6058
Karielly <kariellyycarvalho@gmail.com>
2024-09-04 15:22:04 -0300

    chore: Update navbar to display "Home" instead of "Navbar"

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -3,9 +3,9 @@ import styles from "@/styles/navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className={`navbar w-100 ${styles.navbar}`}>
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">Navbar</span>
+    <nav className={`navbar  ${styles.navbar}`}>
+      <div className="container-fluid d-flex align-items-center justify-content-center">
+        <span className="navbar-brand mb-0 h1">Home</span>
       </div>
     </nav>
   );
